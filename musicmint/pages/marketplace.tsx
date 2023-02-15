74b344d3869e4b98c0ff54efaba8c7fa5dc31577
Luka Gurgenidze <lukagurgenidze@Lukas-MacBook-Air.local>
2023-02-15 10:39:20 -0600

    add example marketplace page and its styling file

---
+++ musicmint/pages/marketplace.tsx
@@ -0,0 +1,36 @@
+import styles from '../styles/marketplace.module.css'
+
+export default function Home({data, error}) {
+  console.log('data :>> ', data)
+  console.log('error :>> ', error)
+
+  return (
+    <div className={styles.marketplaceWrapper}> 
+        <div className={styles.marketplaceTitle}> MarketPlace</div>
+        <div className={styles.marketplaceDescription}> You are on a marketplace page. Welcome!</div>
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  let error = null
+  let data = []
+  
+  try {
+    const response = await fetch(`${process.env.BASE_URL}/smth`)
+    data = await response.json();
+    console.log("data is ")
+    console.log(data)
+  }
+  catch (err) {
+    console.log("error :>> ", err)
+    error = err.message ? err.message : "Something went wrong"
+  }
+
+  return {
+    props: {
+      data: data,
+      error: error,
+    }
+  }
+}
