e7a7f92039936259db1bc69738b75f30126eb9c8
sarahserrosmyers <sarah.a.myers@vanderbilt.edu>
2023-02-20 11:25:09 -0600

    updates to homepage, new css file needed

--- musicmint/pages/index.tsx
+++ musicmint/pages/index.tsx
@@ -3,7 +3,29 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { useContext } from 'react'
 import AuthContext from '../src/context/auth'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/marketplace.module.css'
+
+// [AI-generated, Chatgpt]
+// Define an array of example users to print out on page
+const exampleUsers = [
+  {
+    id: 1,
+    name: 'John Smith',
+    bio: 'I am a musician and producer from Los Angeles. I specialize in electronic music and have released several albums over the years.',
+    nftName: 'Sunset Groove',
+    nftDescription: 'A one-of-a-kind track that will take you on a journey through the sounds of the ocean and the sunset.',
+    nftImage: '/nft1.jpg',
+  },
+  {
+    id: 2,
+    name: 'Emily Johnson',
+    bio: 'I am a visual artist and photographer based in New York City. I love to capture the beauty of nature and urban landscapes.',
+    nftName: 'Cityscape',
+    nftDescription: 'A stunning photograph of the New York City skyline at night, taken from a rooftop in Brooklyn.',
+    nftImage: '/nft2.jpg',
+  },
+  // Add more users as needed
+]
 
 export default function Home({data, error}) {
   // console.log('data :>> ', data)
@@ -12,15 +34,74 @@ export default function Home({data, error}) {
 
 
   return (
+
     <>
       {user ? (
-           <p  onClick={logoutUser}>Logout</p>
-      ): (
-          <Link href="/auth" >Login</Link>
+        <p onClick={logoutUser}>Logout</p>
+      ) : (
+        <Link href="/auth">Login</Link>
       )}
-      <h1>MUSIC MINT</h1>
-      <div> This is test data, please ignore the relevance</div>
-      <h2>-------- DATA BELOW --------</h2>
+
+      <div className={styles.container}>
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <a href="#Logo">Logo</a>
+            </li>
+            <li>
+              <a href="#For Artists">For Artists</a>
+            </li>
+            <li>
+              <a href="#Marketplace">Marketplace</a>
+            </li>
+            <li>
+              <a href="#Search For Artists">Search For Artists</a>
+            </li>
+          </ul>
+        </nav>
+
+        <Head>
+          <title>MUSIC MINT MARKETPLACE</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <main className={styles.main}>
+          <h1 className={styles.title}>MUSIC MINT MARKETPLACE</h1>
+
+          <div className="secondarySection" style={{ marginTop: '6rem' }}>
+            <h2 className={styles.secondaryTitle}>Who are we</h2>
+            <p className={styles.description}>
+              We are a community-driven NFT marketplace that is focused on supporting musicians and creators. Our platform
+              enables artists to sell their unique creations as NFTs, while providing fans with a new way to engage with
+              their favorite artists.
+            </p>
+
+            <h2 className={styles.secondaryTitle}>You can support your favorite creators by:</h2>
+            <p className={styles.description}>
+              Buying their NFTs, sharing their work with others, and following them on social media. By doing so, you help
+              to build a community that supports musicians and creators.
+            </p>
+
+            <h2 className={styles.secondaryTitle}>Support their journey: Cash out when they get famous</h2>
+            <p className={styles.description}>
+              We believe that artists should be rewarded for their hard work and creativity. That's why we offer a
+              cash-out feature that enables artists to benefit from their success. When an artist's work gains popularity,
+              they can cash out their earnings and use the funds to continue their creative journey.
+            </p>
+
+            <h2 className={styles.secondaryTitle}>Start Creating</h2>
+            <p className={styles.description}>
+              Are you a musician or creator looking for a new way to connect with your fans? Join our platform and start
+              creating and selling your NFTs today.
+            </p>
+          </div>
+        </main>
+      </div>
+
+
+      
+
+
       {error && <p>{JSON.stringify(error)}</p>}
       <div>
         {data.map((element: any) => 
@@ -37,6 +118,7 @@ export default function Home({data, error}) {
 
     </>
   )
+  // [AI-generated, Chatgpt]
 }
 
 export async function getStaticProps() {
