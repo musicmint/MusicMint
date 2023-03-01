6f10905f4f4c0aae2f4bd451053ca9657ebd1c11
breidsa <a.breidsp@gmail.com>
2023-03-01 16:07:30 -0600

    cd spinner

--- musicmint/components/Auth/examplebadge.tsx
+++ musicmint/components/Auth/examplebadge.tsx
@@ -1,5 +1,5 @@
 import React, {useContext} from 'react'
-import styles from '../../styles/examplebadge.module.css'
+import '../../styles/examplebadge.module.css'
 import Link from 'next/link'
 import AuthContext from '../../src/context/auth'
 import Image from 'next/image'
@@ -9,16 +9,17 @@ const ExampleBadge = (props) => {
     let { loginUser } = useContext(AuthContext)
 
     return (
-        <div className={styles.usercard}>
-            <div className="user-image">
-                <Image src={anneliese} alt="logo" width={204} height={252}/>
-            </div>
-            <div className="user-info">
-                <h3 className="user-name">Username</h3>
-                <button className="follow-button">Follow</button>
-            </div>
+        <div className="user-card" style={{ border: '1px solid #000', borderRadius: '5px', padding: '10px', boxSizing: 'border-box' }}>
+          <div className="user-image">
+            <Image src={anneliese} alt="logo" width={204} height={252}/>
+          </div>
+          <div className="user-info">
+            <h3 className="user-name">Username</h3>
+            <button className="follow-button">Follow</button>
+          </div>
         </div>
-    )
+      )
+      
 }
 
 export default ExampleBadge
