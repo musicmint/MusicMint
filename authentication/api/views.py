f67bc13580490a77eaf5719e2b86887beed3b2ca
Luka Gurgenidze <lukagurgenidze@Lukas-MacBook-Air.local>
2023-03-21 23:04:13 -0500

    remove username from encoded jwt token

--- authentication/api/views.py
+++ authentication/api/views.py
@@ -19,7 +19,7 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         token = super().get_token(user)
 
         # Add custom claims
-        token['username'] = user.username
+
         # ...
 
         return token
