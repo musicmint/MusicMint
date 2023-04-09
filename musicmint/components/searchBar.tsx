3c8fb419d03ea329d30d0730514a1acdab256705
Luka Gurgenidze <lukagurgenidze@Lukas-MacBook-Air-3.local>
2023-04-09 17:49:21 -0500

    search function done

--- musicmint/components/searchBar.tsx
+++ musicmint/components/searchBar.tsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect} from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import styles from '../styles/componentStyles/searchBar.module.css'
 import Link from 'next/link'
 import AuthContext from '../src/context/auth'
@@ -6,8 +6,65 @@ import Image from 'next/image'
 import logo from '../music-mint-marketplace.png'
 
 const searchBar = (props, { nft, marketplace }) => {
+    let [artistsList, setArtistsList] = useState<any>(null)
+
+
+    useEffect(() => {
+        getArtistList()
+    }, [])
+
+    useEffect(() => {
+        if (artistsList) {
+            const searchInput = document.getElementById('artistSearch') as HTMLInputElement;
+            searchInput.addEventListener('input', filterOnKeyPress);
+            return () => {
+                searchInput.removeEventListener("input", filterOnKeyPress);
+            };
+        }
+    }, [artistsList])
+
+    let filterOnKeyPress = async (event: Event) => {
+        const searchQuery = (event.target as HTMLInputElement).value;
+        let filteredObjects = filterObjectsBySearchQuery(searchQuery);
+
+        console.log(filteredObjects);
+        // Anneliese Take it from here, just use the filtereObjects to display the search results
+    }
+
+
+    let filterObjectsBySearchQuery = (query: string) => {
+        const filteredObjects = Object.keys(artistsList).filter((artist) => {      
+            if (typeof artist === 'string' && artist.toLowerCase().includes(query.toLowerCase())) {
+              return true;
+            }
+      
+          return false;
+        });
+      
+        return filteredObjects;
+      }
+      
+
+    let getArtistList = async () => {
+        let response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/artists/list`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+
+        let data = await response.json()
+
+        if (await response.status === 200) {
+            await setArtistsList(data)
+            console.log(data);
+        } else {
+            console.log("could not fetch artists");
+        }
+    }
+
     return (
-        <input type="text" placeholder="Find your favorite artist" className = {styles.search} ></input>
+        <input type="text" placeholder="Find your favorite artist" className = {styles.search} id="artistSearch"></input>
     )
 }
 
