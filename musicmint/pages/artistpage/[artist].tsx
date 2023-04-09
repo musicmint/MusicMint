8ab82cfb45c9b83095ed74a936eda206627a63fa
Luka Gurgenidze <lukagurgenidze@Lukas-MacBook-Air-3.local>
2023-04-09 15:47:30 -0500

    prelim artistpage routing

---
+++ musicmint/pages/artistpage/[artist].tsx
@@ -0,0 +1,44 @@
+
+import styles from '../../styles/AuthPage.styles/auth.module.css';
+import NavBar from '../../components/navbar';
+import { useRouter } from 'next/router';
+
+
+export default function ArtistPage({ }) {
+    const router = useRouter()
+    const artist = router.query.artist
+
+    return (
+        <>
+        <div className={styles.artistWrapper}> 
+          <NavBar/>
+        </div>
+  
+        <div className={styles.container}>
+          <main className={styles.main}>
+            <div className={styles.artistProfile}>
+              <img className={styles.artistProfileImage} src="https://dummyimage.com/200x200/000/fff" alt="Artist profile" />
+              <h1 className={styles.artistProfileName}>ARTIST NAME: {artist}</h1>
+              <p className={styles.artistProfileBio}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla luctus augue id nisi semper, sit amet hendrerit tortor ultricies.
+              </p>
+              
+            </div>
+  
+  
+            <div className={styles.collectibles}>
+            <div className={styles.authPageDivider}>New Collection</div>
+              <h2 className={styles.collectiblesTitle}>Collectibles</h2>
+              <div className={styles.collectiblesList}>
+                <img className={styles.collectibleImage} src="https://dummyimage.com/150x150/000/fff" alt="Collectible 1" />
+                <img className={styles.collectibleImage} src="https://dummyimage.com/150x150/000/fff" alt="Collectible 2" />
+                <img className={styles.collectibleImage} src="https://dummyimage.com/150x150/000/fff" alt="Collectible 3" />
+                <img className={styles.collectibleImage} src="https://dummyimage.com/150x150/000/fff" alt="Collectible 4" />
+                <img className={styles.collectibleImage} src="https://dummyimage.com/150x150/000/fff" alt="Collectible 5" />
+              </div>
+            </div>
+          </main>
+        </div>
+      </>
+    );
+}
