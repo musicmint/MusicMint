9c15cce7961cd68ccedf9195dca22d361c3f2d02
Luka Gurgenidze <lukagurgenidze@Lukas-MacBook-Air-3.local>
2023-04-10 13:15:59 -0500

    playing with cloudinary

--- artists/views.py
+++ artists/views.py
@@ -5,7 +5,15 @@ from rest_framework.decorators import api_view
 from .models import Artist
 from users.models import User
 # Create your views here.
+from cloudinary.uploader import upload
+from cloudinary.utils import cloudinary_url
 
+cloudinary.config(
+  cloud_name = "dmp0j7nv1",
+  api_key = "224289483296343",
+  api_secret = "-RE2WnfqmkMj09wIXheJgRY4XyA",
+  secure = true
+)
 
 
 
@@ -36,6 +44,12 @@ def update_artist(request):
     new_bio = request.data["new_bio"]
 
     artist = Artist.objects.filter(artist_name=artist_name).first()
+
+
+    upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg", public_id="olympic_flag")
+    url, options = cloudinary_url("olympic_flag", width=100, height=150, crop="fill")
+
+
     if artist is not None:
         artist.artist_bio = new_bio
         artist.save()
--- nnybackend/settings.py
+++ nnybackend/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'drf_yasg',
     'users',
     'artists',
+    'cloudinary',
 ]
 
 REST_FRAMEWORK = {
