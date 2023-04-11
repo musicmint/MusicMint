1b7e04c5f9720206909809673a3186d8c299d1ae
Luka Gurgenidze <lukagurgenidze@Lukas-MacBook-Air-3.local>
2023-04-10 21:07:12 -0500

    add spotify_id field to the artist

--- artists/models.py
+++ artists/models.py
@@ -8,6 +8,7 @@ class Artist(models.Model):
     artist_endpoint = models.TextField(default="")
     artist_bio = models.TextField(default="")
     image_url = models.TextField(default="")
+    spotify_id = models.TextField(default="")
 
     class Meta:
         verbose_name_plural = "Artists"
@@ -20,5 +21,6 @@ class Artist(models.Model):
             "artist_name": self.artist_name,
             "artist_endpoint": self.artist_endpoint,
             "artist_bio": self.artist_bio,
-            "image_url": self.image_url
+            "image_url": self.image_url,
+            "spotify_id": self.spotify_id
         }
