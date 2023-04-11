3314267a7c91d6c8e70b5f474fad9cc2899d5a2b
sarahserrosmyers <sarah.a.myers@vanderbilt.edu>
2023-04-10 20:22:40 -0500

    userprofilecss

--- musicmint/pages/userProfile.tsx
+++ musicmint/pages/userProfile.tsx
@@ -1,18 +1,57 @@
-import React, {useContext, useEffect} from 'react'
+import React, { useContext, useEffect } from 'react'
 import styles from '../styles/pageStyles/userpage.module.css'
 import NavBar from '../components/navbar'
-
+import Banner from '../components/artistBanner';
+import { Row, Form, Button } from 'react-bootstrap'
+import ExampleBadge from '../components/examplebadge'
 
 // ADD PROPS FOR SPECIFIC ARTISTS!!!!!
 
 export default function UserPage(nft, marketplace) {
 
     return (
-        <div className = {styles.container}>
-            <NavBar className={styles.marketNav} nft={nft} marketplace={marketplace} />
-     
-            <p>USER PROFILE!!!!!</p>
+
+        <div className={styles.container}>
+            <div className={styles.main}>
+                <div style={{ zIndex: 2 }}>
+                    <NavBar className={styles.marketNav} nft={nft} marketplace={marketplace} />
+      
+
+                </div>
+
+                <div className={styles.artistProfile}>
+                    <div className={styles.profPlaceholder}></div>
+                    <div className={styles.overlay}></div>
+                    <div className={styles.nameSection}>
+                        <div className={styles.artistProfileName}>YOUR NAME</div>
+                        <div className={styles.bannerContainer}><Banner /></div>
+                    </div>
+                </div>
+
+                <div className={styles.formControlWrapper}>
+                    
+                <div className={styles.collectibles}>
+                    <h2 className={styles.collectiblesTitle}>Your Avaliable Collectibles</h2>
+                    {/* <div className={styles.collectiblesList}> */}
+                    <div className={styles.artistBadges}>
+                        <ExampleBadge />
+                        <ExampleBadge />
+                        <ExampleBadge />
+                        <ExampleBadge />
+                    </div>
+
+
+                </div>
+                </div>
+
+
+                
+            </div>
+
+
+
         </div>
+
     )
 }
 
