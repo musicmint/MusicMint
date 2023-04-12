58e18d7193b464048c6a84781b87bf620eed8b68
breidsa <a.breidsp@gmail.com>
2023-04-12 15:15:51 -0500

    no more circle stretching

--- musicmint/components/Banners/visibleBanner.tsx
+++ musicmint/components/Banners/visibleBanner.tsx
@@ -3,12 +3,25 @@ import React, {useContext, useEffect} from 'react'
 import styles from '../../styles/componentStyles/banner.module.css'
 import Wallet from '../Auth/connectWallet'
 
+
+function formatNumber(num) {
+    if (num === null || num === undefined) {
+        return "N/A";
+      } else if (num >= 1000000) {
+        return (num/1000000).toFixed(1) + "M";
+      } else if (num > 9999) {
+        return (num/1000).toFixed(1) + "k";
+      } else {
+        return num;
+      }
+  }
+
 const VisibleBanner = (props) => {
     return (
         <div className={styles.visibleBannerContainer}>
             <div className={styles.sections}>
                 <div className={styles.followerSectionV}> 
-                    <p className={styles.txt}>{props.followers} Followers</p>
+                    <p className={styles.txt}>{formatNumber(props.followers)} Followers</p>
                 </div>
                 <div className={styles.availSection}>
                     <p className={styles.txt}>0 Available Collectibles</p>
--- musicmint/components/allArtistsSection/banner.tsx
+++ musicmint/components/allArtistsSection/banner.tsx
@@ -2,11 +2,29 @@ import Link from 'next/link'
 import React, { useEffect } from 'react'
 import styles from '../../styles/componentStyles/artistSectionStyles/banner.module.css'
 
+
+function formatNumber(num) {
+    if (num === null || num === undefined) {
+        return "N/A";
+      } else if (num >= 1000000) {
+        return (num/1000000).toFixed(1) + "M";
+      } else if (num > 9999) {
+        return (num/1000).toFixed(1) + "k";
+      } else {
+        return num;
+      }
+  }
+  
 const Banner = ({artist}) => {
   useEffect(()=>{
     console.log(artist);
     
   })
+
+//   useEffect(() => {
+    
+//     })
+
     return (
       // whole badge
       <Link href={`/artistpage/${artist.link}`}>
@@ -15,7 +33,7 @@ const Banner = ({artist}) => {
                 <img className={styles.pic} src={artist.image}></img>
                 <div className={styles.name}>{artist.name}</div>
             </div>
-            <div className={styles.info}>{artist.followers}</div>
+            <div className={styles.info}>{formatNumber(artist.followers)}</div>
             {/* <div className={styles.info}>{artist.volume}</div> */}
         </div>
       </Link>
